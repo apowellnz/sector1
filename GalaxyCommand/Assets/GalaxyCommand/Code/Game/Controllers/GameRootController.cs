7b953e01d4265fc49b3d33a72fda1fccde19ba64
Anthony Powell <apowell@barfoot.co.nz>
2018-06-04 19:36:26 +1200

    Initital Commit

---
+++ GalaxyCommand/Assets/GalaxyCommand/Code/Game/Controllers/GameRootController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameRootController : NetworkBehaviour {
+
+    public GameObject Grid { get; set; }
+
+	// Use this for initialization
+	void Start () {
+
+        Grid.transform.position = Vector3.zero;
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
